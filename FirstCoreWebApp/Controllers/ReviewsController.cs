1bd667e78ce9c725f0f7b7e0674b6e592070f75e
Teresia Gable <45754965+teresiagable@users.noreply.github.com>
2019-12-20 10:49:51 +0100

    tuesdays changes

---
+++ FirstCoreWebApp/Controllers/ReviewsController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FirstCoreWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstCoreWebApp.Controllers
+{
+    public class ReviewsController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.ReviewList = Review.reviewsList;
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(string info)
+        {
+            if (string.IsNullOrWhiteSpace(info))
+            {
+                ViewBag.Msg = "You must enter some text.";
+                return View();
+            }
+            else
+            {
+                Review.reviewsList.Add(new Review() { Info = info });
+                return RedirectToAction("Index");
+            }
+
+        }
+    }
+
+}
---
+++ FirstCoreWebApp/Models/Review.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstCoreWebApp.Models
+{
+    public class Review
+    {
+        static int idCounter = 0;
+        public static List<Review> reviewsList = new List<Review>();
+
+        public int Id { get; set; }
+        public string Info { get; set; }
+
+        public Review()
+        {
+            Id = ++idCounter;
+        }
+    }
+}
